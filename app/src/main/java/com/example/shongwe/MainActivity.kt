5d1c49b707ca21a9fed7235acf74e33eec30fb55
SHONGWE <khethelolungisani83@gmail.com>
2024-06-06 21:52:31 +0200

    first commit

---
+++ app/src/main/java/com/example/shongwe/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.shongwe
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    //declare a variable for birthdate and initialize
+    private val birthdate = 15
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+            findViewById<TextView>(R.id.MainButton)
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
